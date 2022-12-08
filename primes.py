e7a5a1536bd5f770af8c9bd84eb4bb7a2704da6d
Abdullah Beg <abdullahbeg@outlook.com>
2022-12-08 20:06:16 +0000

    Solution

--- primes.py
+++ primes.py
@@ -3,4 +3,28 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError()
+
+    else:
+        counter = 0
+        num = 1
+
+        while(counter != number_of_primes):
+            num += 1
+            holder = num - 1
+            found = False
+
+            while (holder > 1):
+                if (num % holder == 0):
+                    found = True
+                    break
+                else:
+                    holder -= 1
+
+            if found == False:
+                list.append(num)
+                counter += 1
+
     return list
